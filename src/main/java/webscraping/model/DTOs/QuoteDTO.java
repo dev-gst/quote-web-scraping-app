d89e0640a1a443a7488d15b9ca7e83d6bd913478
Gustavo Garcia <gst.garc@gmail.com>
2024-10-12 16:54:30 -0300

    feat: add DTOs classes

---
+++ src/main/java/webscraping/model/DTOs/QuoteDTO.java
@@ -0,0 +1,14 @@
+package webscraping.model.DTOs;
+
+public class QuoteDTO {
+
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
