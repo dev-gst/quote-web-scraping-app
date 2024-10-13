01677ccc4e2f9424e66f4f9fce0938cd70060ecf
Gustavo Garcia <gst.garc@gmail.com>
2024-10-13 14:43:53 -0300

    fix: removed unnecessary access modifier from interface methods and unused import

--- src/main/java/webscraping/repositories/interfaces/DAO.java
+++ src/main/java/webscraping/repositories/interfaces/DAO.java
@@ -2,6 +2,6 @@ package webscraping.repositories.interfaces;
 
 public interface DAO<Entity, DTO> {
 
-    public Entity getById(int id);
-    public int save(DTO dto);
+    Entity getById(int id);
+    int save(DTO dto);
 }
--- src/main/java/webscraping/services/interfaces/Service.java
+++ src/main/java/webscraping/services/interfaces/Service.java
@@ -1,9 +1,7 @@
 package webscraping.services.interfaces;
 
-import webscraping.model.DTOs.AuthorDTO;
-
 public interface Service<Entity, DTO> {
 
-    public Entity getById(int id);
-    public int save(DTO dto);
+    Entity getById(int id);
+    int save(DTO dto);
 }
